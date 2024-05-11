8312e1a8a612b316f923e5fc755d100b62b288ee
Алиев Турач Сураддин-оглы <alievts@mosreg.ru>
2024-05-11 19:17:18 +0300

    revert: init variable

--- app/shared/component/tabs/service/tabs.service.ts
+++ app/shared/component/tabs/service/tabs.service.ts
@@ -1,28 +1,18 @@
-import {Injectable, OnInit} from '@angular/core';
+import {Injectable} from '@angular/core';
 import {TabComponent} from "../component/tab/tab.component";
 import {BehaviorSubject, combineLatest, Observable, ReplaySubject} from "rxjs";
 import {TabTitleComponent} from "../component/tab-title/tab-title.component";
 import {map} from "rxjs/operators";
 
 @Injectable()
-export class TabsService implements OnInit {
-  private tabList: ReplaySubject<TabComponent[]>;
-  public tabList$: Observable<TabComponent[]>;
-  private activeTabTitle: BehaviorSubject<TabTitleComponent>;
-  public activeTabTitle$: Observable<TabTitleComponent | null>;
+export class TabsService {
+  private tabList = new ReplaySubject<TabComponent[]>(1);
+  public tabList$ = this.tabList.asObservable();
+  private activeTabTitle = new BehaviorSubject<TabTitleComponent>(null);
+  public activeTabTitle$ = this.activeTabTitle.asObservable();
   public activeTab$ = this.createActiveTab(this.activeTabTitle$, this.tabList$);
 
   constructor() {}
-
-  public ngOnInit() {
-    const tabList = new ReplaySubject<TabComponent[]>(1);
-    const activeTabTitle = new BehaviorSubject<TabTitleComponent>(null)
-    this.tabList = tabList;
-    this.tabList$ = tabList.asObservable();
-    this.activeTabTitle = activeTabTitle;
-    this.activeTabTitle$ = activeTabTitle.asObservable();
-    this.activeTab$ = this.createActiveTab(this.activeTabTitle$, this.tabList$);
-  }
 
   public updateTabList(tabList: Array<TabComponent>) {
     this.tabList.next(tabList);
